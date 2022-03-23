4d7a79fc18d1c2633edcef537795c41ad8917b90
Akira <78860065+Akira034567@users.noreply.github.com>
2022-03-23 13:08:59 -0300

    Create codigo4.ino

---
+++ Exercícios/codigo4.ino
@@ -0,0 +1,24 @@
+const int led = 3;
+const int botao = 5;
+
+void setup()
+{
+  pinMode(led, OUTPUT);
+  pinMode(botao, INPUT);
+}
+
+int var;
+
+void loop()
+{
+  var = digitalRead(botao);
+  
+  if(var == 1) {
+    digitalWrite (led, HIGH);
+  } else {
+    digitalWrite (led, LOW);
+  }
+
+
+}
+  
